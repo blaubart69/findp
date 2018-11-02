a330c5a5024dd45f648a1c4b75a03ee47db7894e
Bernhard Spindler <bernhard.spindler@unicredit.at>
2018-11-02 13:52:50 +0100

    when no dir is given the dot is the default

--- findp/getopts.cpp
+++ findp/getopts.cpp
@@ -2,25 +2,22 @@
 
 void PrintUsage(void);
 
+WCHAR g_dotDir[2];
+
 int getopts(int argc, wchar_t *argv[], Options* opts)
 {
-	 if (argc < 2)
-	 {
-		 return 1;
-	 }
-
-	 opts->sum = false;
-	 opts->progress = false;
-	 opts->maxDepth = -1;
-	 opts->followJunctions = false;
-	 opts->FilenameSubstringPattern = NULL;
-	 opts->ThreadsToUse = 32;
-	 opts->SumUpExtensions = false;
-	 opts->ExtsFilename = NULL;
-	 opts->rootDir = NULL;
-	 opts->printFull = false;
-	 opts->printOwner = false;
-	 opts->emit = EmitType::Files;
+	opts->sum = false;
+	opts->progress = false;
+	opts->maxDepth = -1;
+	opts->followJunctions = false;
+	opts->FilenameSubstringPattern = NULL;
+	opts->ThreadsToUse = 32;
+	opts->SumUpExtensions = false;
+	opts->ExtsFilename = NULL;
+	opts->rootDir = NULL;
+	opts->printFull = false;
+	opts->printOwner = false;
+	opts->emit = EmitType::Files;
 
 	 bool showHelp = false;
 	 LPCWSTR tmpEmitType = NULL;
@@ -72,9 +69,13 @@ int getopts(int argc, wchar_t *argv[], Options* opts)
 
 	 if (opts->rootDir == NULL)
 	 {
+		 /*
 		 Log::Instance()->err(L"no directory given");;
 		 PrintUsage();
 		 return 2;
+		 */
+		 lstrcpyW(g_dotDir, L".");
+		 opts->rootDir = g_dotDir;
 	 }
 
 	 if (opts->FilenameSubstringPattern != NULL)
@@ -109,7 +110,7 @@ void PrintUsage(void)
 		L"\n  -h              ... show this help"
 		L"\n  -t {f|d|b}      ... emit what  (files|directory|both) default: files"
 		L"\n  -m {pattern}	  ... substring to match within filename. case insensitive"
-		L"\n  -d {depth}      ... how many directory to go down"
+		L"\n  -d {depth}      ... how many directories to go down"
 		L"\n  -x {threads}	  ... threads to start for parallel enumerations"
 		L"\n"
 		L"\nprepend   \\\\?\\   if you want to have long path support."
